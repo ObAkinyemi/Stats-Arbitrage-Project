1dcc9e1684fb708182c18634d9f98ec88667ff54
ObAkinyemi <beteiscoo@gmail.com>
2026-02-16 11:43:05 -0700

    I have figured out how to now write to a csv and the data looks relatively good. However, I need to get the data to be ticker a, b, c, d, e, f..., b, c, d, e, f..., c, d, e, f... to e,f... Then put the p-values under each of the corresponding pairs, the hedge_ratio, and the half life under that as well. So a top down structure like this: ticker, data, p-value, hedge ratio, half life. I also need to learn more about what the  Augmented Dickey-Fuller (ADF) Test, Engle-Granger Test actually are and what they do, so I'll do that.

--- project-1/main.py
+++ project-1/main.py
@@ -1,14 +1,13 @@
 import yfinance as yf
 import pandas as pd
-
+import csv
 
 tickerList = ['ES=F', 'YM=F', 'CL=F', 'NG=F', 'GC=F', 'HG=F', '6E=F', '6J=F', 'ZB=F', 'ZN=F']
-
 # auto_adjust = true gives me all the adjusted close data (I think)
 rawData = yf.download(tickers=tickerList, period="2y", interval="1d", auto_adjust=True)
 closeData = rawData['Close']
 # print(DownloadedData)
-print(rawData['Close']['ES=F'].iloc[1])
+print(rawData['Close']['ES=F'].keys())
 # Close, Open, 
 # print(rawData['Close'])
 
@@ -33,11 +32,18 @@ i = 0
 j = 0
 
 # this is the logic for the comparison
-for i in range(i, len(tickerList)):
-    # print("Ticker i data: " + str(i) + str(closeData[tickerList[i]]))
-    for j in range(i+1,len(tickerList)-1):
-        # Now loop through the price data using the length of the
-        # rawData['Close']['ES=F'].iloc[k] and pit the tickers against each other
-        print(f"ticker i: {tickerList[i]} vs ticker j: {tickerList[j]}")
-        for k in range(0, len(rawData['Close'][tickerList[i]])-495):
-            print(f"{rawData['Close'][tickerList[i]].iloc[k]} vs {rawData['Close'][tickerList[j]].iloc[k]}")
+# for i in range(i, len(tickerList)):
+#     # print("Ticker i data: " + str(i) + str(closeData[tickerList[i]]))
+    
+#     for j in range(i+1,len(tickerList)-1):
+#         # Now loop through the price data using the length of the
+#         # rawData['Close']['ES=F'].iloc[k] and pit the tickers against each other
+       
+#         print(f"ticker i: {tickerList[i]} vs ticker j: {tickerList[j]}")
+#         with open('test.csv', 'a', newline='') as csvfile:
+#             writer = csv.writer(csvfile)
+#             writer.writerow([tickerList[i], tickerList[j]])
+        
+#             for k in range(0, len(rawData['Close'][tickerList[i]])-495):
+#                 print(f"{rawData['Close'][tickerList[i]].iloc[k]} vs {rawData['Close'][tickerList[j]].iloc[k]}")
+#                 writer.writerow([rawData['Close'][tickerList[i]].iloc[k], rawData['Close'][tickerList[j]].iloc[k]])
