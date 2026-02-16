46ed40fe2a5172f1c76c7148b98af6a76078a25f
ObAkinyemi <beteiscoo@gmail.com>
2026-02-15 23:13:16 -0700

    figured out how to stack tickers against each other. Now I need to put it into csv format, so I can do maths on it. But the groundwork is done. I just needed iloc and I was chillin. The data type of the data is a dataframe with a series in it. It's like a dictionary filled with arrays that aren't really arrays. The data structure - top level: pandas data frame below that

--- project-1/main.py
+++ project-1/main.py
@@ -1,18 +1,20 @@
 import yfinance as yf
 import pandas as pd
 
+
 tickerList = ['ES=F', 'YM=F', 'CL=F', 'NG=F', 'GC=F', 'HG=F', '6E=F', '6J=F', 'ZB=F', 'ZN=F']
 
 # auto_adjust = true gives me all the adjusted close data (I think)
 rawData = yf.download(tickers=tickerList, period="2y", interval="1d", auto_adjust=True)
 closeData = rawData['Close']
 # print(DownloadedData)
-# print(type(rawData))
+print(rawData['Close']['ES=F'].iloc[1])
 # Close, Open, 
 # print(rawData['Close'])
 
 #loop through i (the tickers) done
-# loop through j (the price data)
+# loop through j (the the comparative tickers)
+# loop through k (the price data using iloc)
 # Create a 2D array with the ticker data with the following structure
     # [
         # [ticker 1 data, ticker 2 data], [ticker 1 data, ticker 2 data]
@@ -34,7 +36,8 @@ j = 0
 for i in range(i, len(tickerList)):
     # print("Ticker i data: " + str(i) + str(closeData[tickerList[i]]))
     for j in range(i+1,len(tickerList)-1):
-        # print("J: " + str(j) + "and I: " + str(i))
-        print("Ticker " + tickerList[i] + " vs Ticker" + tickerList[j])
-        
-# Now
+        # Now loop through the price data using the length of the
+        # rawData['Close']['ES=F'].iloc[k] and pit the tickers against each other
+        print(f"ticker i: {tickerList[i]} vs ticker j: {tickerList[j]}")
+        for k in range(0, len(rawData['Close'][tickerList[i]])-495):
+            print(f"{rawData['Close'][tickerList[i]].iloc[k]} vs {rawData['Close'][tickerList[j]].iloc[k]}")
