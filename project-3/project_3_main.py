bde49edd0c214aa2e18074e952b2b6f015d3d79d
ObAkinyemi <beteiscoo@gmail.com>
2026-06-14 18:29:44 -0600

    forgot to commit project 2 changes earlier. it works now. I forgot what was broken (max drawdown probably???). Just starting project 3.

---
+++ project-3/project_3_main.py
@@ -0,0 +1,8 @@
+import yfinance as yf
+import pandas as pd
+import statsmodels.api as sm
+from statsmodels.tsa.stattools import adfuller
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import sys
